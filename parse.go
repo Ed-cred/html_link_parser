361069e74d4ec6b47217305b3a2dc768d0b61ac1
Ed-cred <edwardandrew63@gmail.com>
2023-09-25 11:32:56 +0300

    dfs the node tree

--- parse.go
+++ parse.go
@@ -1,6 +1,11 @@
 package link
 
-import "io"
+import (
+	"fmt"
+	"io"
+
+	"golang.org/x/net/html"
+)
 
 // Link represent a link (<a href="...">) in an html document
 type Link struct {
@@ -9,5 +14,21 @@ type Link struct {
 }
 
 func Parse(r io.Reader) ([]Link, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+	dfs(doc, "")
 	return nil, nil
+}
+
+func dfs(node *html.Node, padding string) {
+	msg := node.Data
+	if node.Type == html.ElementNode {
+		msg = "<" + msg + ">"
+	}
+	fmt.Println(padding, msg)
+	for c := node.FirstChild; c != nil; c = c.NextSibling {
+		dfs(c, padding+"  ")
+	}
 }
